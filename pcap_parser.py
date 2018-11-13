cc1cb4f22c067b075e55711968902ea43839e3fc
Eric Hu <ericc.hu@mail.utoronto.ca>
2018-11-13 18:45:39 -0500

    counts number of TCP and UDP packets

--- pcap_parser.py
+++ pcap_parser.py
@@ -6,9 +6,18 @@ from scapy.all import *
 
 packets = rdpcap('univ1_pt16')
 counter = 0
+tcp_count = 0
+udp_count = 0
 
 for packet in packets:
     counter += 1
+    if packet.haslayer(TCP):
+        tcp_count += 1
+
+    if packet.haslayer(UDP):
+        udp_count += 1
 
 
+print(tcp_count)
+print(udp_count)
 print(counter)

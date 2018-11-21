9ded002c757cb167092ce5ea9aaad319cdf49363
Eric Hu <ericc.hu@mail.utoronto.ca>
2018-11-21 16:56:37 -0500

    added timestamp flow

--- pcap_parser.py
+++ pcap_parser.py
@@ -29,6 +29,21 @@ def partitionFile():
 #packets = rdpcap('univ1_pt16')
 
 
+class Flow:
+    def __init__(self, count, ts):
+        self.count = count
+        self.ts = [ts]
+
+    def increaseCount(self, count):
+        self.count += count
+
+    def addTs(self, timestamp):
+        self.ts.append(timestamp)
+
+    def newFlowPacket(self, count, timeStamp):
+        self.increaseCount(count)
+        self.addTs(timeStamp)
+
 
 def plotCDF(step, packetSize, headerSize):
     x_data = []
@@ -247,6 +262,17 @@ def count_protocols():
     print("total number of packets: " + str(counter) + "\n")
 
 
+def closestTimeStamp(list, currTime):
+    result = -1
+
+    for i in range(len(list)):
+        if currTime - list[i].ts[-1] < 5400:
+            result = i
+            break
+
+    return result
+
+
 
 def plotFlow(flowDict, max, step, protocol, flowType):
     x_data = []
@@ -259,18 +285,19 @@ def plotFlow(flowDict, max, step, protocol, flowType):
             y_data.append(0)
 
     for key in flowDict:
-        counter = 0
+        for i in range(len(flowDict[key])):
+            counter = 0
 
-        for size in x_data:
-            if flowDict[key] <= size:
-                y_data[counter] += 1
+            for size in x_data:
+                if flowDict[key][i].count <= size:
+                    y_data[counter] += 1
 
-            counter += 1
+                counter += 1
 
-    y_data[:] = [x/967143 for x in y_data]
+    y_data[:] = [x/len(flowDict) for x in y_data]
 
     plt.xlabel('max '+ protocol +' flow size by ' + flowType + ' count')
-    plt.ylabel('number of ' + protocol + ' ' + flowType)
+    plt.ylabel('percentage of ' + protocol + ' flows')
     plt.plot(x_data, y_data)
     plt.savefig("flow size of " + protocol + " " + flowType + " count.png")
     plt.clf()
@@ -311,7 +338,6 @@ def flow_rebuild():
 
     for wirelen, ts, packet in pkts:
         counter += 1
-
         eth=dpkt.ethernet.Ethernet(packet)
 
         if eth.type==dpkt.ethernet.ETH_TYPE_IP:
@@ -320,6 +346,7 @@ def flow_rebuild():
 
             if ip.p == dpkt.ip.IP_PROTO_TCP:
                 tcp_count += 1
+                tcp = ip.data
                 # if not ((packet[IP].src,packet[IP].dst) in TCP_flow):
                 #     TCP_flow[(packet[IP].src,packet[IP].dst)] = [[],[],[]]
 
@@ -327,43 +354,69 @@ def flow_rebuild():
                 # TCP_flow[(packet[IP].src,packet[IP].dst)][0].append(packet[TCP].time)
                 # TCP_flow[(packet[IP].src,packet[IP].dst)][1].append(wirelen)
                 # TCP_flow[(packet[IP].src,packet[IP].dst)][2].append(packet[TCP].flags)
-                if not ((ip.src, ip.dst) in TCP_flow_pc) or ((ip.dst, ip.src) in TCP_flow_pc):
-                    TCP_flow_pc[(ip.src, ip.dst)] = 0
-                    TCP_flow_bs[(ip.src, ip.dst)] = 0
-                    TCP_flow_f[(ip.src, ip.dst)] = []
+                if not (((ip.src, ip.dst, tcp.sport, tcp.dport) in TCP_flow_pc) or ((ip.dst, ip.src, tcp.dport, tcp.sport) in TCP_flow_pc)):
+                    TCP_flow_pc[(ip.src, ip.dst, tcp.sport, tcp.dport)] = [Flow(1, ts)]
+                    TCP_flow_bs[(ip.src, ip.dst, tcp.sport, tcp.dport)] = [Flow(wirelen, ts)]
+                    TCP_flow_f[(ip.src, ip.dst, tcp.sport, tcp.dport)] = []
                     TCP_flow_dir=[]
-                elif (ip.src, ip.dst) in TCP_flow_pc:
-                    TCP_flow_pc[(ip.src, ip.dst)] += 1
-                    TCP_flow_bs[(ip.src, ip.dst)] += wirelen
-                    TCP_flow_f[(ip.src, ip.dst)].append(ip.data.flags)
-                    TCP_flow_dir.append((ip.src, ip.dst))
+                elif (ip.src, ip.dst, tcp.sport, tcp.dport) in TCP_flow_pc:
+                    index = closestTimeStamp(TCP_flow_pc[(ip.src, ip.dst, tcp.sport, tcp.dport)], ts)
+                    if index == -1:
+                        TCP_flow_pc[(ip.src, ip.dst, tcp.sport, tcp.dport)].append(Flow(1, ts))
+                        TCP_flow_bs[(ip.src, ip.dst, tcp.sport, tcp.dport)].append(Flow(wirelen, ts))
+                        index = len(TCP_flow_pc[(ip.src, ip.dst, tcp.sport, tcp.dport)]) - 1
+
+                    #index = 0
+                    TCP_flow_pc[(ip.src, ip.dst, tcp.sport, tcp.dport)][index].newFlowPacket(1, ts)
+                    TCP_flow_bs[(ip.src, ip.dst, tcp.sport, tcp.dport)][index].newFlowPacket(wirelen, ts)
+                    TCP_flow_f[(ip.src, ip.dst, tcp.sport, tcp.dport)].append(ip.data.flags)
+                    TCP_flow_dir.append((ip.src, ip.dst, tcp.sport, tcp.dport))
                 else:
-                    TCP_flow_pc[(ip.dst, ip.src)] += 1
-                    TCP_flow_bs[(ip.dst, ip.src)] += wirelen
-                    TCP_flow_f[(ip.dst, ip.src)].append(ip.data.flags)
-                    TCP_flow_dir.append((ip.src, ip.dst))
+                    index = closestTimeStamp(TCP_flow_pc[(ip.dst, ip.src, tcp.dport, tcp.sport)], ts)
+                    if index == -1:
+                        TCP_flow_pc[(ip.dst, ip.src, tcp.dport, tcp.sport)].append(Flow(1, ts))
+                        TCP_flow_bs[(ip.dst, ip.src, tcp.dport, tcp.sport)].append(Flow(wirelen, ts))
+                        index = len(TCP_flow_pc[(ip.dst, ip.src, tcp.dport, tcp.sport)]) - 1
+                    #index = 0
+                    TCP_flow_pc[(ip.dst, ip.src, tcp.dport, tcp.sport)][index].newFlowPacket(1, ts)
+                    TCP_flow_bs[(ip.dst, ip.src, tcp.dport, tcp.sport)][index].newFlowPacket(wirelen, ts)
+                    TCP_flow_f[(ip.dst, ip.src, tcp.dport, tcp.sport)].append(ip.data.flags)
+                    TCP_flow_dir.append((ip.src, ip.dst, tcp.dport, tcp.sport))
 
 
             elif ip.p == dpkt.ip.IP_PROTO_UDP:
                 udp_count += 1
+                udp = ip.data
                 # if not ((packet[IP].src,packet[IP].dst) in UDP_flow):
                 #     UDP_flow[(packet[IP].src,packet[IP].dst)] = [[],[]]
                 # # UDP_flow[(packet[IP].src,packet[IP].dst)].append([packet[UDP].time,wirelen])
                 # UDP_flow[(packet[IP].src,packet[IP].dst)][0].append(packet[UDP].time)
                 # UDP_flow[(packet[IP].src,packet[IP].dst)][1].append(wirelen)
 
-                if not ((ip.src, ip.dst) in UDP_flow_pc) or ((ip.dst, ip.src) in UDP_flow_pc):
-                    UDP_flow_pc[(ip.src, ip.dst)] = 0
-                    UDP_flow_bs[(ip.src, ip.dst)] = 0
+                if not (((ip.src, ip.dst, udp.sport, udp.dport) in UDP_flow_pc) or ((ip.dst, ip.src, udp.dport, udp.sport) in UDP_flow_pc)):
+                    UDP_flow_pc[(ip.src, ip.dst, udp.sport, udp.dport)] = [Flow(1, ts)]
+                    UDP_flow_bs[(ip.src, ip.dst, udp.sport, udp.dport)] = [Flow(wirelen, ts)]
                     UDP_flow_dir=[]
-                elif (ip.src, ip.dst) in UDP_flow_pc:
-                    UDP_flow_pc[(ip.src, ip.dst)] += 1
-                    UDP_flow_bs[(ip.src, ip.dst)] += wirelen
-                    UDP_flow_dir.append((ip.src, ip.dst))
+                elif (ip.src, ip.dst, udp.sport, udp.dport) in UDP_flow_pc:
+                    index = closestTimeStamp(UDP_flow_pc[(ip.src, ip.dst, udp.sport, udp.dport)], ts)
+                    if index == -1:
+                        UDP_flow_pc[(ip.src, ip.dst, udp.sport, udp.dport)].append(Flow(1, ts))
+                        UDP_flow_bs[(ip.src, ip.dst, udp.sport, udp.dport)].append(Flow(wirelen, ts))
+                        index = len(UDP_flow_pc[(ip.src, ip.dst, udp.sport, udp.dport)]) - 1
+
+                    UDP_flow_pc[(ip.src, ip.dst, udp.sport, udp.dport)][index].newFlowPacket(1, ts)
+                    UDP_flow_bs[(ip.src, ip.dst, udp.sport, udp.dport)][index].newFlowPacket(wirelen, ts)
+                    UDP_flow_dir.append((ip.src, ip.dst, udp.sport, udp.dport))
                 else:
-                    UDP_flow_pc[(ip.dst, ip.src)] += 1
-                    UDP_flow_bs[(ip.dst, ip.src)] += wirelen
-                    UDP_flow_dir.append((ip.dst, ip.src))
+                    index = closestTimeStamp(UDP_flow_pc[(ip.dst, ip.src, udp.dport, udp.sport)], ts)
+                    if index == -1:
+                        UDP_flow_pc[(ip.dst, ip.src, udp.dport, udp.sport)].append(Flow(1, ts))
+                        UDP_flow_bs[(ip.dst, ip.src, udp.dport, udp.sport)].append(Flow(wirelen, ts))
+                        index = len(UDP_flow_pc[(ip.dst, ip.src, udp.dport, udp.sport)]) - 1
+
+                    UDP_flow_pc[(ip.dst, ip.src, udp.dport, udp.sport)][index].newFlowPacket(1, ts)
+                    UDP_flow_bs[(ip.dst, ip.src, udp.dport, udp.sport)][index].newFlowPacket(wirelen, ts)
+                    UDP_flow_dir.append((ip.dst, ip.src, udp.dport, udp.sport))
 
     #TCP_flow_pc.values()
     #TCP_flow_bs.values()
@@ -371,11 +424,11 @@ def flow_rebuild():
     #UDP_flow_pc.values()
     #UDP_flow_bs.values()
 
-    plotFlow(TCP_flow_pc, 1000, 10, "TCP", "packet")
-    plotFlow(TCP_flow_bs, 40000, 10, "TCP", "byte")
+    plotFlow(TCP_flow_pc, 500, 10, "TCP", "packet")
+    plotFlow(TCP_flow_bs, 400000, 100, "TCP", "byte")
 
-    plotFlow(UDP_flow_pc, 1000, 10, "UDP", "packet")
-    plotFlow(UDP_flow_bs, 40000, 10, "UDP", "byte")
+    plotFlow(UDP_flow_pc, 50, 1, "UDP", "packet")
+    plotFlow(UDP_flow_bs, 4000, 1, "UDP", "byte")
 
 
 
